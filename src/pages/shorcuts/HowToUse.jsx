6039afcf665f6ccb593579ef7140ae188c02cbaa
Hyeseon Lee <hyesun1999@gmail.com>
2023-05-31 09:13:21 +0900

    default

--- src/pages/shorcuts/HowToUse.jsx
+++ src/pages/shorcuts/HowToUse.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function HowToUse() {
-  return <div>사용법을 주루루루룩</div>;
+  return (
+    <div className="w-full h-full flex flex-col p-3 justify-content items-center">
+      <div className="flex text-2xl font-semibold mb-3 bg-lime-200 px-2 py-1">
+        ⯎ 버튼을 클릭해서 단축어를 다운로드 받아보세요 ⯎
+      </div>
+    </div>
+  );
 }
